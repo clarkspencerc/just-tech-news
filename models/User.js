dc38966c62853ffd2a5e81f5ffd01c482a59bdab
clarkspencerc <clarkspencerc@gmail.com>
2022-08-11 21:13:35 -0600

    login logout function

--- models/User.js
+++ models/User.js
@@ -1,51 +1,63 @@
-const { Model, DataTypes } = require('sequelize'); 
-const sequelize = require('../config/connection.js'); 
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
 
-// create our User model 
-class User extends Model {} 
+// create our User model
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
-// define table columns and configuration 
+// create fields/columns for User model
 User.init(
     {
         id: {
-            type: DataTypes.INTEGER, 
-            allowNull: false, 
-            primaryKey: true, 
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
             autoIncrement: true
-        }, 
+        },
         username: {
             type: DataTypes.STRING,
             allowNull: false
         },
         email: {
             type: DataTypes.STRING,
-            allowNull: false, 
-            unique: true, 
+            allowNull: false,
+            unique: true,
             validate: {
                 isEmail: true
             }
-        }, 
+        },
         password: {
             type: DataTypes.STRING,
-            allowNull: false, 
+            allowNull: false,
             validate: {
                 len: [4]
             }
         }
-    }, 
+    },
     {
-        // Table configuration options go here 
-        // pass in our imported sequelize connection (the direct connection to our database)
-        sequelize, 
-        // don't automatically create createdAt/updatedAt timestamp fields 
-        timestamps: false, 
-        // done pluralize name of database table 
-        freezeTableName: true, 
-        // use underscores instead of camel casing
-        underscored: true, 
-        // make it so our model name stays lowercase in the database 
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
         modelName: 'user'
     }
-); 
+);
 
-module.exports = User; 
+module.exports = User;
